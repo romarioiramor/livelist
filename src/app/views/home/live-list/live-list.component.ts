90f163dc798dddf229128be6e7cd658da111ed28
romario <romario.adsistemas@gmail.com>
2020-07-03 16:56:55 -0300

    ajustes lives

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,7 @@
+import { Live } from './../../../shared/model/live.model';
+import { LiveService } from './../../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: Live[];
+  liveNext: Live[];
+
+  constructor(
+    public liveService: LiveService,
+    public sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+    this.getLives();
+  }
+
+  getLives() {
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+      this.livesPrevious.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
+    });
+
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+      this.liveNext = data.content;
+      console.log(this.liveNext);
+      this.liveNext.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
+    });
   }
 
 }
